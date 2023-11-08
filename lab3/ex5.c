e3354c6452a4ead3e2e6c070a14d67f990e616e7
Dimitris Skondras Mexis <mitsosskme@gmail.com>
2023-11-08 05:27:57 +0200

    added ex5

---
+++ lab3/ex5.c
@@ -0,0 +1,12 @@
+/*  
+    κάνουμε compile γράφοντας στο terminal: gcc -o ex5 ex5.c 
+    τρέχουμε το πρόγραμμα μας γράφοντας στο terminal: ./ex5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\nworld\n");
+    printf("at\nd.i.t\n");
+}
