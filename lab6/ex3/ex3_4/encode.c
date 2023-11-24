5e48fd17db6daa8a676c8199166a7b81b4e0d978
dimskomex <mitsosskme@gmail.com>
2023-11-24 21:02:14 +0200

    added full ex3

---
+++ lab6/ex3/ex3_4/encode.c
@@ -0,0 +1,18 @@
+/*
+    ./encode < text > encode_text
+*/
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int ch;                             /* Be careful! Declare ch as int because of getchar() and EOF */
+    ch = getchar();                     /* Read first character */
+    while (ch != EOF) {                 /* Go on if we didn't reach end of file */
+        if (ch >= 'a' && ch <= 'z')     /* If lower case letter */
+            ch++;
+
+        putchar(ch);                    /* Print out character */
+        ch = getchar();                 /* Read next character */
+    }
+}
