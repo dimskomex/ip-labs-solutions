e525d55c029490400275df809375524e883b6e62
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-10-17 19:17:15 +0300

    Initial Commit

---
+++ Lab 0/hello_blink.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Εκτύπωση του "Hello world" με αναβόσβησμα
+    printf("Hello \033[5mworld\033[5m!\n");
+
+    return 0;
+}
+
+// Μεταγλώττιση:
+// gcc hello_blink.c -o hello_blink
+// Εκτέλεση:
+// ./hello_blink
+// Έξοδος:
+// Hello world! (με αναβόσβησμα)
+// Σημείωση: Για να δείτε το αναβόσβημα, θα πρέπει να τρέξετε το πρόγραμμα σε κονσόλα (terminal) και όχι σε κάποιο περιβάλλον ανάπτυξης (π.χ. Visual Studio Code).
