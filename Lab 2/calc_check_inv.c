705dd36c37f8bb5a156aef350bea1fe852a8a704
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-11-25 19:22:21 +0200

    added pyth.c and reformatted with google

--- Lab 2/calc_check_inv.c
+++ Lab 2/calc_check_inv.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    long long int num1, num2;
-    char operation;
+  long long int num1, num2;
+  char operation;
 
-    // Ζητάμε τον πρώτο αριθμό
-    printf("Please enter the first number: ");
-    if (scanf("%lld", &num1) != 1) {
-        fprintf(stderr, "Error: Invalid input for the first number.\n");
-        return 1;
-    }
+  // Ζητάμε τον πρώτο αριθμό
+  printf("Please enter the first number: ");
+  if (scanf("%lld", &num1) != 1) {
+    printf("Error: Invalid input for the first number.\n");
+    return 1;
+  }
 
-    // Ζητάμε τον δεύτερο αριθμό
-    printf("Please enter the second number: ");
-    if (scanf("%lld", &num2) != 1) {
-        fprintf(stderr, "Error: Invalid input for the second number.\n");
-        return 1;
-    }
+  // Ζητάμε τον δεύτερο αριθμό
+  printf("Please enter the second number: ");
+  if (scanf("%lld", &num2) != 1) {
+    printf("Error: Invalid input for the second number.\n");
+    return 1;
+  }
 
-    // Ζητάμε την πράξη που θέλει ο χρήστης
-    printf("Please enter the operation (+ for addition, - for subtraction): ");
-    scanf(" %c", &operation);  // To κενό διάστημα πριν το %c αγνοεί το newline χαρακτήρα
+  // Ζητάμε την πράξη που θέλει ο χρήστης
+  printf("Please enter the operation (+ for addition, - for subtraction): ");
+  scanf(" %c",
+        &operation);  // To κενό διάστημα πριν το %c αγνοεί το newline χαρακτήρα
 
-    // Υπολογισμός ανάλογα με την πράξη
-    if (operation == '+') {
-        printf("The result of the addition is: %lld\n", num1 + num2);
-    } else if (operation == '-') {
-        printf("The result of the subtraction is: %lld\n", num1 - num2);
-    } else {
-        fprintf(stderr, "Error: Invalid operation. Please enter + or -.\n");
-        return 1;
-    }
+  // Υπολογισμός ανάλογα με την πράξη
+  if (operation == '+') {
+    printf("The result of the addition is: %lld\n", num1 + num2);
+  } else if (operation == '-') {
+    printf("The result of the subtraction is: %lld\n", num1 - num2);
+  } else {
+    printf("Error: Invalid operation. Please enter + or -.\n");
+    return 1;
+  }
 
-    return 0;  // Επιστροφή επιτυχίας
+  return 0;  // Επιστροφή επιτυχίας
 }
-
 
 // Μεταγλώττιση:
 // gcc calc_check_inv.c -o calc_check_inv
