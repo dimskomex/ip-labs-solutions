a9f1dea70d1e04ddbf57b5fa8a98131da51f8bfc
anastasia <ammarinakou@gmail.com>
2025-11-17 00:29:31 +0200

    add to lab 2 and 3 solutions

---
+++ lab2/calc.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+    printf("================= BASIC SOLUTION =================\n\n\n");
+    // Basic solution
+    int n1, n2;
+
+    // printf("Please enter the first number: ");
+    // scanf("%d", &n1);
+
+    // printf("\nPlease enter the second number: ");
+    // scanf("%d", &n2);
+
+    // printf("\nThe sum of the two numbers is: %d\n", n1 + n2);
+
+    printf("\n\n\n================= ADVANCED 1 =================\n\n\n");
+
+    // // Advanced 1
+    // long long int ln1, ln2;
+
+    // printf("Please enter the first number: ");
+    // scanf("%lld", &ln1);
+
+    // printf("\nPlease enter the second number: ");
+    // scanf("%lld", &ln2);
+
+    // printf("\nThe sum of the two numbers is: %lld\n", ln1 + ln2);
+
+    printf("\n\n\n================= ADVANCED 2 =================\n\n\n");
+
+    // Advanced 2
+    /*
+        Η υλοποίηση ζητάει και τον τελεστή της πράξης. Πριν τυπώσει
+        την απάντηση, ελέγχει εάν ο τελεστής είναι '+' ή '-' ώστε να
+        εκτελεστεί η σωστή πράξη.
+    */
+    // char op;
+
+    // printf("Please enter the first number: ");
+    // scanf("%d", &n1);
+
+    // printf("\nPlease enter operation: ");
+    // scanf(" %c", &op);
+
+    // printf("\nPlease enter the second number: ");
+    // scanf("%d", &n2);
+
+    // if (op == '+') {
+    //     printf("\nThe sum of the two numbers is: %d\n", n1 + n2);
+    // } else {
+    //     printf("The difference of the two numbers is: %d", n1 - n2);
+    // }
+
+    printf("\n\n\n================= ADVANCED 3 =================\n\n\n");
+
+    // Advanced 3
+    int t;
+    printf("Please enter the first number: ");
+
+    t = getchar();
+    while (t != EOF) {
+        if (t >= 48 && t <= 58) {
+
+        } else if (t == 10 || t == )
+    }
+    return 0;
+}
